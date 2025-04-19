1890bcd756ef0b5a9f93d824b9b047ac984eda51
muhammadali0308 <abdukarimovm.ali12@gmail.com>
2025-04-19 10:51:38 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Abdukarimov Muhammadali</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus deleniti quod illo vero saepe molestias modi enim sunt? Explicabo culpa porro, reprehenderit fuga iure voluptates. Quasi a totam, facere odio ad fugiat quisquam aspernatur, deleniti saepe officia corrupti aut similique at dolore praesentium cumque! Doloribus illo tempore nulla quaerat error! Mollitia quibusdam et culpa minima omnis! Incidunt quae minus veritatis ab, harum hic explicabo odit. Accusamus quaerat porro expedita iste illum similique dicta tempora veritatis ipsam sint!</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Tailwind css</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
